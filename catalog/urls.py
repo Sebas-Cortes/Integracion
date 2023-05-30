6ff199f74cee93f94f06a5a8754cc5bb7aebf820
Iguana Obesa <101284398+IguanaObesa@users.noreply.github.com>
2023-05-29 21:02:35 -0400

    29/05 9:05
    login pagina, css y menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import menu_template, medicamento_template, receta_template
+from .views import login,menu_medico, medicamento_template, receta_template
 
 
 urlpatterns = [
-    path('', menu_template, name="menu_template"),
+    path('', login, name="login"),
+    path('menu_medico', menu_medico, name="menu_medico"),
     path('medicamento_template', medicamento_template, name="medicamento_template"),
     path('receta_template', receta_template, name="receta_template"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def menu_template(request):
-    return render(request, 'catalog/menu_template.html')
+def login(request):
+    return render(request, 'catalog/login.html')
+def menu_medico_template(request):
+    return render(request, 'catalog/menu_medico.html')
 def medicamento_template(request):
     return render(request, 'catalog/medicamento_template.html')
 def receta_template(request):
