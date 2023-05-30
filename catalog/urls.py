2d638337b968054d0e46859915253488111e2418
luismd1 <lu.munozd@duocuc.cl>
2023-05-30 02:31:15 -0400

    no pude hacer el makemigrations

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from . import views
-from .views import login,menu_medico, medicamento_template, receta_template
+from .views import login,menu_medico, medicamento, receta_template, receta, medicamento_template
 
 
 urlpatterns = [
     path('', login, name="login"),
     path('menu_medico', menu_medico, name="menu_medico"),
-    path('medicamento_template', medicamento_template, name="medicamento_template"),
+    path('medicamento_template', medicamento_template, name="medicamento_template"),    
+    path('medicamento', medicamento, name="medicamento"),
+    path('menu_medico', receta_template, name="menu_medico"),
     path('receta_template', receta_template, name="receta_template"),
+    path('receta', receta, name="receta"),    
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'catalog/login.html')
-def menu_medico_template(request):
-    return render(request, 'catalog/menu_medico.html')
+
 def medicamento_template(request):
     return render(request, 'catalog/medicamento_template.html')
+
+def medicamento(request):
+    return render(request, 'catalog/medicamento.html')
+
+def menu_medico(request):
+    return render(request, 'catalog/menu_medico.html')
+
 def receta_template(request):
     return render(request, 'catalog/receta_template.html')
+
+def receta(request):
+    return render(request, 'catalog/receta.html')
