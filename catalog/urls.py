1b55ccb904c2a7a534eea61000762445c642aec0
Iguana Obesa <101284398+IguanaObesa@users.noreply.github.com>
2023-05-30 22:19:50 -0400

    30-05/ 10:15
    mejora templates + paginas del medico, falta editar prescripcion

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,24 +1,12 @@
 from django.urls import path
-<<<<<<< Updated upstream
-from .views import login,menu_medico, medicamento, receta_template, receta, medicamento_template
-=======
+
 from . import views
 from .views import login,menu_medico, medicamento, prescripcion_crear,ver_pres
->>>>>>> Stashed changes
-
 
 urlpatterns = [
     path('', login, name="login"),
     path('menu_medico', menu_medico, name="menu_medico"),
-<<<<<<< Updated upstream
-    path('medicamento_template', medicamento_template, name="medicamento_template"),    
-    path('medicamento', medicamento, name="medicamento"),
-    path('menu_medico', receta_template, name="menu_medico"),
-    path('receta_template', receta_template, name="receta_template"),
-    path('receta', receta, name="receta"),    
-=======
     path('medicamento', medicamento, name="medicamento"),
     path('prescripcion_crear', prescripcion_crear, name="prescripcion_crear"),
     path('ver_pres', ver_pres, name="ver_pres"),
->>>>>>> Stashed changes
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,23 +4,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'catalog/login.html')
-<<<<<<< Updated upstream
-
-def medicamento_template(request):
-    return render(request, 'catalog/medicamento_template.html')
-
-def medicamento(request):
-    return render(request, 'catalog/medicamento.html')
-
-def menu_medico(request):
-    return render(request, 'catalog/menu_medico.html')
-
-def receta_template(request):
-    return render(request, 'catalog/receta_template.html')
-
-def receta(request):
-    return render(request, 'catalog/receta.html')
-=======
 def menu_medico(request):
     return render(request, 'catalog/menu_medico.html')
 def medicamento(request):
@@ -29,4 +12,4 @@ def prescripcion_crear(request):
     return render(request, 'catalog/prescripcion_crear.html')
 def ver_pres(request):
     return render(request, 'catalog/ver_pres.html')
->>>>>>> Stashed changes
+
